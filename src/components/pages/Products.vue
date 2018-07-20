780da4f9f19c75045949dc53cdbd113f11cf77db
Corey <corey_924@yahoo.com.tw>
2018-07-20 16:53:20 +0800

    Init jquery

--- src/components/pages/Products.vue
+++ src/components/pages/Products.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div class="text-right mt-4">
-      <button class="btn btn-primary">建立新商品</button>
+      <button class="btn btn-primary" @click="openModal">建立新商品</button>
     </div>
     <table class="table mt-4">
       <thead>
@@ -34,11 +34,32 @@
         </tr>
       </tbody>
     </table>
+    <!-- Modal -->
+    <div class="modal fade" id="productModal" tabindex="-1" role="dialog" aria-labelledby="productModalLabel" aria-hidden="true">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="productModalLabel">Modal title</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            ...
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary">Save changes</button>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 
 </template>
 
 <script>
+  import $ from 'jquery';
   export default{
     data(){
       return{
@@ -55,7 +76,10 @@
         this.$http.get(api).then((response) => {
           vm.products = response.data.products;
         });
+      },
+      openModal(){
+        $('#productModal').modal('show');
       }
-    }
+    },
   }
 </script>
